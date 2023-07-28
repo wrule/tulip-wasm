3775c6c70a326a626f6b66e12488a0cf924122df
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-28 21:44:19 +0800

    测试代码

--- src/dev.ts
+++ src/dev.ts
@@ -4,9 +4,11 @@ import { init } from './tulipx_promise';
 
 async function dev() {
   await init();
-  console.log(start(72, [10]));
+  const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const list2 = Array(10000000).fill(0).map(() => Math.random() * 1000);
+  // console.log(start(72, [10]));
   console.log(
-    run(72, [[1, 2, 3, 4, 5]], [3], 2)
+    run(48, [list1], [2, 3, 2], true)
   );
 }
 
