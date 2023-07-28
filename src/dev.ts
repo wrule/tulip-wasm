2704de4663f203a9b2e1ee1f18b1816adbc4def5
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-28 22:01:36 +0800

    性能测试

--- src/dev.ts
+++ src/dev.ts
@@ -6,10 +6,9 @@ async function dev() {
   await init();
   const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const list2 = Array(10000000).fill(0).map(() => Math.random() * 1000);
-  // console.log(start(72, [10]));
-  console.log(
-    run(48, [list1], [2, 3, 2], true)
-  );
+  const old_time = Date.now();
+  const result = run(72, [list2], [10]);
+  console.log(Date.now() - old_time);
 }
 
 dev();
