739dc8fe70d76fa93365d06908dcfc2e108c428d
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-28 18:23:53 +0800

    start复制过来

---
+++ src/single.ts
@@ -0,0 +1,11 @@
+
+export
+function start(indic_index: number, options: ArrayLike<number>) {
+  const tulipx: TulipX = Global.tulipx_wasm;
+  const task = tulipx._push(indic_index, 0, 1);
+  tulipx._set_array(tulipx._options(task), options);
+  tulipx._run(task);
+  const outputs_offset = tulipx._outputs_offset(task);
+  tulipx._pop();
+  return outputs_offset;
+}
