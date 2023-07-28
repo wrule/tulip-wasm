7aa6b438cfb52616af8b5a7982a3bf3e20c5a1dc
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-28 21:38:43 +0800

    测试代码

--- src/dev.ts
+++ src/dev.ts
@@ -1,10 +1,13 @@
 import { hello } from '.';
-import { start } from './single';
+import { start, run, _align } from './single';
 import { init } from './tulipx_promise';
 
 async function dev() {
   await init();
   console.log(start(72, [10]));
+  console.log(
+    run(72, [[1, 2, 3, 4, 5]], [3], 2)
+  );
 }
 
 dev();
