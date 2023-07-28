ff7e28043537b7d87fc28db2075acfced10e46a2
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-28 20:50:00 +0800

    Global.tulip_wasm

--- src/single.ts
+++ src/single.ts
@@ -3,7 +3,7 @@ import { TulipX } from './tulipx_promise';
 
 export
 function start(indic_index: number, options: ArrayLike<number>) {
-  const tulipx: TulipX = Global.tulipx_wasm;
+  const tulipx: TulipX = Global.tulip_wasm;
   const task = tulipx._push(indic_index, 0, 1);
   tulipx._set_array(tulipx._options(task), options);
   tulipx._run(task);
