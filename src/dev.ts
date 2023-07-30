e4b69f6e3f59c5afd5ac6e669f9a500a410ba441
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-30 21:57:10 +0800

    修改为es2016以支持终结器实验

--- src/dev.ts
+++ src/dev.ts
@@ -1,6 +1,8 @@
 import * as t from '.';
 
 async function dev() {
+  console.log(FinalizationRegistry);
+  return;
   await t.init();
   const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const list2 = Array(100000).fill(0).map(() => Math.random() * 1000);
