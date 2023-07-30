18dde4936ec70bfca90134524ae08a22ad66e365
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-30 19:20:57 +0800

    测试代码

--- src/dev.ts
+++ src/dev.ts
@@ -8,6 +8,12 @@ async function dev() {
   const old_time = Date.now();
   const seq = t.sequence(() => {
     const rsi = t.rsi_q(list2, 45);
+    const sma1 = t.sma_q(rsi.outputs.rsi, 21);
+    const sma2 = t.sma_q(rsi.outputs.rsi, 36);
+    const diff = t.sub_q(sma1.outputs.sma, sma2.outputs.sma);
+    const k = t.sma_q(diff.outputs.sub, 40);
+    const d = t.sma_q(k.outputs.sma, 34);
+
     return t.stoch_q(rsi.outputs.rsi, rsi.outputs.rsi, rsi.outputs.rsi, {
       k_period: 45,
       k_slowing_period: 13,
