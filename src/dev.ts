b3b2ef3315327e64a69a5ccf9926901577e50b24
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-30 18:57:51 +0800

    测试代码修改

--- src/dev.ts
+++ src/dev.ts
@@ -1,13 +1,13 @@
-import * as tulip from '.';
+import * as t from '.';
 
 async function dev() {
-  await tulip.init();
+  await t.init();
   const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const list2 = Array(10000000).fill(0).map(() => Math.random() * 1000);
 
   const old_time = Date.now();
-  const seq = tulip.sequence(() => {
-    return tulip.sma_q(list2, 3);
+  const seq = t.sequence(() => {
+    return t.sma_q(list2, 3);
   });
   const result = seq.Run();
   console.log(Date.now() - old_time, result.length);
