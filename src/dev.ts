00c570dbf1bf7d7db839b2e0fc765c77f9fd7ac8
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-31 15:33:58 +0800

    测速代码

--- src/dev.ts
+++ src/dev.ts
@@ -3,12 +3,11 @@ import * as t from '.';
 async function dev() {
   await t.init();
   const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const list2 = Array(100000).fill(0).map(() => Math.random() * 1000);
+  const list2 = Array(10000000).fill(0).map(() => Math.random() * 1000);
 
-  setInterval(() => {
-    const a = t.sma(list2, 13);
-    // console.log(a.length);
-  }, 10);
+  console.time('op');
+  const result = t.sma(list2, 10);
+  console.timeEnd('op');
 }
 
 dev();
