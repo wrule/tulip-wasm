0f7bda36edac8e453852602c21bbe856b77df3d7
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-31 12:48:45 +0800

    gc_registry加入

--- src/meta.ts
+++ src/meta.ts
@@ -1,8 +1,17 @@
+import { Global } from './utils';
 import { Indicator } from './indicator';
+import { TulipX } from './tulipx_promise';
 import indicators from './indicators.json';
 
 export
 const documents: Indicator[] = indicators;
+
+export
+const gc_registry = new FinalizationRegistry((ids: number[]) => {
+  const tulipx: TulipX = Global.tulip_wasm;
+  console.log('gc', ids);
+  tulipx._erase_batch(ids[0], ids[ids.length - 1], 0);
+});
 
 export * from './single';
 export * from './sequence';
