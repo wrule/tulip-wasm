5a640b520b906954dd63b955847a5e0129f3ee44
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-31 15:51:23 +0800

    测速

--- src/dev.ts
+++ src/dev.ts
@@ -5,9 +5,11 @@ async function dev() {
   const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const list2 = Array(10000000).fill(0).map(() => Math.random() * 1000);
 
-  console.time('op');
-  const result = t.sma(list2, 10);
-  console.timeEnd('op');
+  while (true) {
+    console.time('op');
+    const result = t.sma(list2, 10);
+    console.timeEnd('op');
+  }
 }
 
 dev();
