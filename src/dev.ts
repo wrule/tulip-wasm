fb520f940575af322554824f6027642716c5d410
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-31 14:21:24 +0800

    测试代码

--- src/dev.ts
+++ src/dev.ts
@@ -1,43 +1,14 @@
 import * as t from '.';
 
-const registry = new FinalizationRegistry((value) => {
-  console.log('垃圾回收了', value);
-});
-
-function func() {
-  const mem = Array(1e4).fill(0).map(() => Math.random());
-  registry.register(mem, 123);
-}
-
 async function dev() {
-  func();
-  let list: number[] = [];
-  setInterval(() => {
-    list.push(...Array(1e4).fill(0).map(() => Math.random()));
-    console.log(list.length);
-  }, 100);
-  return;
   await t.init();
   const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const list2 = Array(100000).fill(0).map(() => Math.random() * 1000);
 
-  const old_time = Date.now();
-  const seq = t.sequence(() => {
-    const rsi = t.rsi_q(list2, 45);
-    const sma1 = t.sma_q(rsi.outputs.rsi, 21);
-    const sma2 = t.sma_q(rsi.outputs.rsi, 36);
-    const diff = t.sub_q(sma1.outputs.sma, sma2.outputs.sma);
-    const k = t.sma_q(diff.outputs.sub, 40);
-    const d = t.sma_q(k.outputs.sma, 34);
-
-    return t.stoch_q(rsi.outputs.rsi, rsi.outputs.rsi, rsi.outputs.rsi, {
-      k_period: 45,
-      k_slowing_period: 13,
-      d_period: 32,
-    });
-  });
-  const result = seq.Run();
-  console.log(Date.now() - old_time, result.stoch_k.length);
+  setInterval(() => {
+    const a = t.sma(list2, 13);
+    // console.log(a.length);
+  }, 10);
 }
 
 dev();
