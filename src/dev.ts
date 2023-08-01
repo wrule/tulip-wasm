6a373ccdf9871b95b49361a809d9be8e5864398d
jimao <jimao@jimaodeMacBook-Pro.local>
2023-08-01 15:20:22 +0800

    测试代码

--- src/dev.ts
+++ src/dev.ts
@@ -5,8 +5,8 @@ async function dev() {
   const list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const list2 = Array(10000000).fill(0).map(() => Math.random() * 1000);
   const a = t.crossaction(
-    [10, 9, 8,  7,  6, 10, 10, 11, 12, 10 ],
-    [ 1, 3, 9, 10, 11, 11, 11, 10, 10, 11 ],
+    [1, 2, 3],
+    [2, 1, 4],
   );
   console.log(a);
   return;
