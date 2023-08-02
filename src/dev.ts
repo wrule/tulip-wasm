13edfd769dce93638c9dfd3c9a8bcae03faff2f8
jimao <jimao@jimaodeMacBook-Pro.local>
2023-08-02 18:13:51 +0800

    测试代码

--- src/dev.ts
+++ src/dev.ts
@@ -4,13 +4,13 @@ async function dev() {
   await t.init();
   const close = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const seq = t.sequence(() => {
-    return t.sma_q(close, 3, 'sma1');
-    // const sma2 = t.sma_q(sma1.inputs.real, 0, 'sma2');
-    // return t.crossroi_q(sma1.outputs.sma, sma2.outputs.sma, sma1.inputs.real, 0.001);
+    const sma1 = t.sma_q(close, 0, 'sma1');
+    const sma2 = t.sma_q(sma1.inputs.real, 0, 'sma2');
+    return t.crossroi_q(sma1.outputs.sma, sma2.outputs.sma, sma1.inputs.real, 0.001);
   });
   seq.Update({
     sma1: { period: 2 },
-    // sma2: { period: 4 },
+    sma2: { period: 4 },
   });
   const roi = seq.Run();
   console.log(roi);
