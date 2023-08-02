15c6638044472f8b1f3832ea5ef5ffc6c5ba2762
jimao <jimao@jimaodeMacBook-Pro.local>
2023-08-02 17:59:57 +0800

    不要输出gc信息以及测试代码

--- src/dev.ts
+++ src/dev.ts
@@ -2,10 +2,23 @@ import * as t from '.';
 
 async function dev() {
   await t.init();
-  const list = Array(10000000).fill(0).map(() => Math.random() * 1000);
-  console.time('sma');
-  const a = t.sma(list, 10);
-  console.timeEnd('sma');
+  const close = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const seq = t.sequence(() => {
+    return t.sma_q(close, 3, 'sma1');
+    // const sma2 = t.sma_q(sma1.inputs.real, 0, 'sma2');
+    // return t.crossroi_q(sma1.outputs.sma, sma2.outputs.sma, sma1.inputs.real, 0.001);
+  });
+  seq.Update({
+    sma1: { period: 2 },
+    // sma2: { period: 4 },
+  });
+  const roi = seq.Run();
+  console.log(roi);
+
+  // const list = Array(10000000).fill(0).map(() => Math.random() * 1000);
+  // console.time('sma');
+  // const a = t.sma(list, 10);
+  // console.timeEnd('sma');
 }
 
 dev();
